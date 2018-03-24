1e44ab1d43f7166307b51558ba8ed6d4f9d05e23
vitaliy <bliznuuk@gmail.com>
2018-03-24 17:27:11 +0200

    Project was generated from maven-archetype-quickstart

---
+++ src/main/java/com/vbliznyuk/edu/java/App.java
@@ -0,0 +1,13 @@
+package com.vbliznyuk.edu.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
