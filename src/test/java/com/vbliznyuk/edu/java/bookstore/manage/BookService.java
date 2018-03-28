c1eda23e22a9b4a38e2608f3d1a445205b9ef627
vitaliy <bliznuuk@gmail.com>
2018-03-28 20:40:51 +0300

    Implement simple BookService class with method createBook(title, price, description)

---
+++ src/test/java/com/vbliznyuk/edu/java/bookstore/manage/BookService.java
@@ -0,0 +1,20 @@
+package com.vbliznyuk.edu.java.bookstore.manage;
+
+import com.vbliznyuk.edu.java.bookstore.book.Book;
+
+public class BookService {
+	
+	public BookService() {
+	}
+	
+	public Book createBook(String title, double price, String description) {
+		Book book = new Book();
+		book.setTitle(title);
+		book.setPrice(price);
+		book.setDescription(description);
+		
+		book.setNumber(""+Math.random());
+		
+		return book;
+	}
+}
