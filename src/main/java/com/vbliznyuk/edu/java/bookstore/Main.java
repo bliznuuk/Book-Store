a6fbf08d68824e14b4888633210488305c6e5fed
vitaliy <bliznuuk@gmail.com>
2018-03-28 21:00:05 +0300

    Implement simple Main class that create BookService and call the createBook method

---
+++ src/main/java/com/vbliznyuk/edu/java/bookstore/Main.java
@@ -0,0 +1,16 @@
+package com.vbliznyuk.edu.java.bookstore;
+
+import com.vbliznyuk.edu.java.bookstore.book.Book;
+import com.vbliznyuk.edu.java.bookstore.manage.BookService;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		BookService bs = new BookService();
+		Book book = bs.createBook("cdi practice", 99.99, "master your knowledge");
+		
+		System.out.println(book.getNumber());
+	}
+
+}
