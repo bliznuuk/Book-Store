255c49de35ef5a7419dd4d0ecf977cedd1dbc6de
vitaliy <bliznuuk@gmail.com>
2018-03-29 21:37:06 +0300

    Created BookServiceTest with two tests methods and one @Before.
    @Test: bookShouldBeCreated() and
    bookServiceShouldCorrectSetUpProperties()

---
+++ src/test/java/com/vbliznyuk/edu/java/bookstore/BookServiceTest.java
@@ -0,0 +1,39 @@
+package com.vbliznyuk.edu.java.bookstore;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.vbliznyuk.edu.java.bookstore.book.Book;
+import com.vbliznyuk.edu.java.bookstore.manage.BookService;
+
+public class BookServiceTest {
+
+	private BookService bookService;
+	private Book testBook = bookService.createBook("test", 999, "test");
+	
+	@Before
+	public void setUp() {
+		bookService = new BookService();
+	}
+
+	@Test
+	public void bookShouldBeCreated(){
+		assertNull("BookService haven't created Book object", testBook);
+	}
+	
+	@Test
+	public void bookServiceShouldCorrectSetUpProperties() {
+		String title = "book Test name!";
+		double price = 123445.67;
+		String description = "Some tiny description for testing correctnesses of creation new Book.";
+
+		Book book = bookService.createBook(title, price, description);
+		
+		assertEquals("Book tittle has been setting up wrong!", book.getTitle(), title);
+		assertEquals("Book price has been setting up wrong", book.getPrice(), price, 0.009);
+		assertEquals("Book description has been setting up wrong!", book.getDescription(), description);
+	}
+}
