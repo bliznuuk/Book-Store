7d49345ae3d6ac8122044e1bcd7a3e4a13e3797f
vitaliy <bliznuuk@gmail.com>
2018-04-04 15:27:57 +0300

    Change exec-plugin configuration for using welds StartMain.class, and add observe method to the Main.class

--- src/main/java/com/vbliznyuk/edu/java/bookstore/Main.java
+++ src/main/java/com/vbliznyuk/edu/java/bookstore/Main.java
@@ -1,16 +1,29 @@
 package com.vbliznyuk.edu.java.bookstore;
 
+import java.util.List;
+
+import javax.enterprise.event.Observes;
+import javax.inject.Singleton;
+
+import org.jboss.weld.environment.se.bindings.Parameters;
+import org.jboss.weld.environment.se.events.ContainerInitialized;
+
 import com.vbliznyuk.edu.java.bookstore.book.Book;
 import com.vbliznyuk.edu.java.bookstore.manage.BookService;
 
+@Singleton
 public class Main {
 
-	public static void main(String[] args) {
+/*	public static void main(String[] args) {
 		
 		BookService bs = new BookService();
 		Book book = bs.createBook("cdi practice", 99.99, "master your knowledge");
 		
 		System.out.println(book);
 	}
-
+*/
+	public void Hello(@Observes ContainerInitialized initEvent, @Parameters List<String> parameters) {
+		System.out.println("Hello from weld!");
+	}
+	
 }
