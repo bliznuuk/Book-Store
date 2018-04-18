dd378d92e0dc4b0d22e11d7669041bc035371e17
vitaliy <bliznuuk@gmail.com>
2018-04-18 20:46:59 +0300

    Create Weld instance and initialize container.
    Obtain BookService instance using weld container.

--- src/main/java/com/vbliznyuk/edu/java/bookstore/Main.java
+++ src/main/java/com/vbliznyuk/edu/java/bookstore/Main.java
@@ -1,16 +1,23 @@
 package com.vbliznyuk.edu.java.bookstore;
 
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+
 import com.vbliznyuk.edu.java.bookstore.book.Book;
 import com.vbliznyuk.edu.java.bookstore.manage.BookService;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Weld weld = new Weld();
+		WeldContainer container = weld.initialize();
 		
-		BookService bs = new BookService();
-		Book book = bs.createBook("cdi practice", 99.99, "master your knowledge");
+		BookService bookService = container.select(BookService.class).get();
+		Book book = bookService.createBook("cdi practice", 99.99, "master your knowledge");
 		
 		System.out.println(book);
+		
+		container.shutdown();
 	}
 
 }
